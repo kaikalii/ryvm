c0d4507136937e0add9c2ce9efb4de43735dcb8c
Kai Schmidt <kaikaliischmidt@gmail.com>
2020-03-18 01:04:38 -0700

    create instruments and mixer

---
+++ src/instrument.rs
@@ -0,0 +1,209 @@
+use std::{
+    f32::consts::PI,
+    ops::{Index, IndexMut},
+    sync::Arc,
+};
+
+use crossbeam::sync::ShardedLock;
+use once_cell::sync::Lazy;
+use rodio::Source;
+
+pub type SampleType = f32;
+
+/// The global sample rate
+pub const SAMPLE_RATE: u32 = 32000;
+
+static SINE_SAMPLES: Lazy<Vec<SampleType>> = Lazy::new(|| {
+    (0..SAMPLE_RATE)
+        .map(|i| (i as SampleType / SAMPLE_RATE as SampleType * 2.0 * PI).sin())
+        .collect()
+});
+
+#[derive(Debug, Clone)]
+pub struct SourceLock<T>(Arc<ShardedLock<T>>);
+
+impl<T> SourceLock<T> {
+    pub fn new(inner: T) -> Self {
+        SourceLock(Arc::new(ShardedLock::new(inner)))
+    }
+    pub fn get<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        f(&*self.0.read().unwrap())
+    }
+    pub fn update<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        f(&mut *self.0.write().unwrap())
+    }
+}
+
+/// An instrument for producing sounds
+#[derive(Debug, Clone)]
+pub enum Instrument {
+    Sine { f: SampleType, i: u32 },
+    Square { f: SampleType, i: u32 },
+    Mixer(Vec<Balanced>),
+}
+
+impl Instrument {
+    pub fn sine(f: SampleType) -> Self {
+        Instrument::Sine { f, i: 0 }
+    }
+    pub fn square(f: SampleType) -> Self {
+        Instrument::Square { f, i: 0 }
+    }
+    #[allow(clippy::should_implement_trait)]
+    pub fn next(&mut self) -> Option<(SampleType, SampleType)> {
+        match self {
+            Instrument::Sine { f, i } => {
+                let s = SINE_SAMPLES[*i as usize];
+                *i = (*i + *f as u32) % SAMPLE_RATE;
+                Some((s, s))
+            }
+            Instrument::Square { f, i } => {
+                let samples_per_cycle = (SAMPLE_RATE as SampleType / *f) as u32;
+                let s = if *i < samples_per_cycle / 2 {
+                    1.0
+                } else {
+                    -1.0
+                } * 0.6;
+                *i = (*i + 1) % samples_per_cycle as u32;
+                Some((s, s))
+            }
+            Instrument::Mixer(list) => {
+                let (left_vol_sum, right_vol_sum) =
+                    list.iter().fold((0.0, 0.0), |(lacc, racc), instr| {
+                        let (l, r) = instr.stereo_volume();
+                        (lacc + l, racc + r)
+                    });
+                let (left_sum, right_sum) =
+                    list.iter_mut().fold((0.0, 0.0), |(lacc, racc), instr| {
+                        let (l, r) = instr.stereo_volume();
+                        if let Some((ls, rs)) = instr.instr.next() {
+                            (lacc + ls * l, racc + rs * r)
+                        } else {
+                            (lacc, racc)
+                        }
+                    });
+                Some((left_sum / left_vol_sum, right_sum / right_vol_sum))
+            }
+        }
+    }
+    pub fn set_freq(&mut self, freq: SampleType) {
+        match self {
+            Instrument::Sine { f, .. } => *f = freq,
+            Instrument::Square { f, .. } => *f = freq,
+            _ => {}
+        }
+    }
+    pub fn source(self) -> (InstrumentSource, SourceLock<Instrument>) {
+        let lock = SourceLock::new(self);
+        let lock_clone = lock.clone();
+        let source = InstrumentSource {
+            inner: lock,
+            queue: None,
+            i: 0,
+        };
+        (source, lock_clone)
+    }
+}
+
+impl Index<usize> for Instrument {
+    type Output = Balanced;
+    fn index(&self, i: usize) -> &Self::Output {
+        if let Instrument::Mixer(list) = self {
+            &list[i]
+        } else {
+            panic!("Attempted to index non-mixer instrument")
+        }
+    }
+}
+
+impl IndexMut<usize> for Instrument {
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        if let Instrument::Mixer(list) = self {
+            &mut list[i]
+        } else {
+            panic!("Attempted to index non-mixer instrument")
+        }
+    }
+}
+
+/// A balance wrapper for an `Instrument`
+#[derive(Debug, Clone)]
+pub struct Balanced {
+    pub instr: Instrument,
+    pub volume: SampleType,
+    pub pan: SampleType,
+}
+
+impl From<Instrument> for Balanced {
+    fn from(instr: Instrument) -> Self {
+        Balanced {
+            instr,
+            volume: 1.0,
+            pan: 0.0,
+        }
+    }
+}
+
+impl Balanced {
+    pub fn stereo_volume(&self) -> (SampleType, SampleType) {
+        (
+            self.volume * (1.0 - self.pan.max(0.0)),
+            self.volume * (1.0 + self.pan.min(0.0)),
+        )
+    }
+    pub fn volume(self, volume: SampleType) -> Self {
+        Balanced { volume, ..self }
+    }
+    pub fn pan(self, pan: SampleType) -> Self {
+        Balanced { pan, ..self }
+    }
+}
+
+/// A wrapper around an `Instrument` to implement `rodio::Source`
+pub struct InstrumentSource {
+    inner: SourceLock<Instrument>,
+    queue: Option<SampleType>,
+    i: u32,
+}
+
+impl Iterator for InstrumentSource {
+    type Item = SampleType;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.queue
+            .take()
+            .map(|sample| {
+                self.i += 1;
+                sample
+            })
+            .or_else(|| {
+                let queue = &mut self.queue;
+                self.inner.update(|inner| {
+                    inner.next().map(|(left, right)| {
+                        *queue = Some(right);
+                        left
+                    })
+                })
+            })
+    }
+}
+
+impl Source for InstrumentSource {
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+    fn channels(&self) -> u16 {
+        2
+    }
+    fn sample_rate(&self) -> u32 {
+        SAMPLE_RATE
+    }
+    fn total_duration(&self) -> std::option::Option<std::time::Duration> {
+        None
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 macro_rules! mods {
-    ($($m:ident),*) => ($(mod $m; use $m::*;)*);
+    ($($m:ident),*) => ($(mod $m; pub use $m::*;)*);
 }
 
-mods!(control);
+mods!(control, instrument);
 
 use piston_window::*;
+use rodio::Sink;
 
 fn main() {
     let mut con = Controller::default();
     let mut window: PistonWindow = WindowSettings::new("Album", con.window_size)
+        .exit_on_esc(true)
         .build()
         .unwrap();
+
+    let device = rodio::default_output_device().unwrap();
+    let sink = Sink::new(&device);
+
+    let (source, mixer) = Instrument::Mixer(vec![
+        Balanced::from(Instrument::square(440.0)).pan(-1.0),
+        Balanced::from(Instrument::square(554.0)).pan(1.0),
+        Balanced::from(Instrument::sine(554.0)).pan(0.0),
+    ])
+    .source();
+    sink.append(source);
+
     // Main loop
     while let Some(event) = window.next() {
         // Draw
@@ -20,10 +34,25 @@ fn main() {
         // Process events
         if let Event::Input(input, _) = event {
             match input {
-                Input::Move(Motion::MouseCursor(pos)) => con.mouse_pos = pos,
+                Input::Move(Motion::MouseCursor(pos)) => {
+                    con.mouse_pos = pos;
+                    const POW_DELTA: SampleType = 0.011;
+                    mixer.update(|mixer| {
+                        mixer[0]
+                            .instr
+                            .set_freq((pos[1] as f32).powf(1.0 + POW_DELTA * 0.0) + 1.0);
+                        mixer[1]
+                            .instr
+                            .set_freq((pos[1] as f32).powf(1.0 + POW_DELTA * 4.0) + 1.0);
+                        mixer[2]
+                            .instr
+                            .set_freq((pos[1] as f32).powf(1.0 + POW_DELTA * 3.0) + 1.0);
+                    });
+                }
                 Input::Resize(args) => con.window_size = args.window_size,
                 _ => {}
             }
         }
+        // Update
     }
 }

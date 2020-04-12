2e7a0bafa1ad2f2a85002dd055e8b6a2da1cffb8
Kai Schmidt <kaikaliischmidt@gmail.com>
2020-04-11 18:22:17 -0700

    remove windowing

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,11 @@ macro_rules! mods {
     ($($m:ident),*) => ($(mod $m; pub use $m::*;)*);
 }
 
-mods!(control, instrument);
+mods!(instrument);
 
-use piston_window::*;
 use rodio::Sink;
 
 fn main() {
-    let mut con = Controller::default();
-    let mut window: PistonWindow = WindowSettings::new("Album", con.window_size)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
     let device = rodio::default_output_device().unwrap();
     let sink = Sink::new(&device);
 
@@ -26,33 +19,8 @@ fn main() {
     sink.append(source);
 
     // Main loop
-    while let Some(event) = window.next() {
-        // Draw
-        window.draw_2d(&event, |_context, graphics, _device| {
-            clear([0.0, 0.0, 0.0, 1.0], graphics);
-        });
-        // Process events
-        if let Event::Input(input, _) = event {
-            match input {
-                Input::Move(Motion::MouseCursor(pos)) => {
-                    con.mouse_pos = pos;
-                    const POW_DELTA: SampleType = 0.011;
-                    mixer.update(|mixer| {
-                        mixer[0]
-                            .instr
-                            .set_freq((pos[1] as f32).powf(1.0 + POW_DELTA * 0.0) + 1.0);
-                        mixer[1]
-                            .instr
-                            .set_freq((pos[1] as f32).powf(1.0 + POW_DELTA * 4.0) + 1.0);
-                        mixer[2]
-                            .instr
-                            .set_freq((pos[1] as f32).powf(1.0 + POW_DELTA * 3.0) + 1.0);
-                    });
-                }
-                Input::Resize(args) => con.window_size = args.window_size,
-                _ => {}
-            }
-        }
+    loop {
+
         // Update
     }
 }

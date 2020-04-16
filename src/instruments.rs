5a9a1eb6680483a55e5c8636c41504f42f20a46a
Kai Schmidt <kaikaliischmidt@gmail.com>
2020-04-15 22:17:07 -0700

    commands can be queued for the next measure

--- src/instruments.rs
+++ src/instruments.rs
@@ -1,12 +1,18 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::{
+    collections::{HashMap, HashSet},
+    iter::repeat,
+    mem::swap,
+};
 
 use indexmap::IndexMap;
 use rodio::Source;
 use serde_derive::{Deserialize, Serialize};
 
+#[cfg(feature = "keyboard")]
+use crate::Keyboard;
 use crate::{
-    Frame, FrameCache, InstrId, Instrument, RyvmApp, SampleType, SourceLock, MAX_BEATS,
-    SAMPLE_EPSILON, SAMPLE_RATE,
+    AddApp, Balance, Frame, FrameCache, InstrId, Instrument, RyvmApp, SampleType, Sampling,
+    SourceLock, WaveForm, MAX_BEATS, SAMPLE_EPSILON, SAMPLE_RATE,
 };
 
 fn default_tempo() -> SampleType {
@@ -26,7 +32,7 @@ pub struct Instruments {
     #[serde(skip)]
     sample_queue: Option<SampleType>,
     #[serde(skip)]
-    command_queue: VecDeque<RyvmApp>,
+    command_queue: Vec<RyvmApp>,
     #[serde(default = "default_tempo", skip_serializing_if = "is_default_tempo")]
     tempo: SampleType,
     #[serde(skip)]
@@ -39,7 +45,7 @@ impl Default for Instruments {
             output: None,
             map: IndexMap::new(),
             sample_queue: None,
-            command_queue: VecDeque::new(),
+            command_queue: Vec::new(),
             tempo: 120.0,
             i: 0,
         }
@@ -57,7 +63,7 @@ impl Instruments {
         self.i
     }
     pub fn measure_i(&self) -> u32 {
-        self.i % (self.samples_per_measure() / MAX_BEATS as f32) as u32
+        self.i % self.samples_per_measure() as u32
     }
     pub fn set_output<I>(&mut self, id: I)
     where
@@ -99,13 +105,118 @@ impl Instruments {
             }
         }
     }
+    pub fn queue_command(&mut self, app: RyvmApp) {
+        self.command_queue.push(app);
+    }
+    fn process_command(&mut self, app: RyvmApp) {
+        match app {
+            RyvmApp::Quit => {}
+            RyvmApp::Output { name } => self.set_output(name),
+            RyvmApp::Tempo { tempo } => self.set_tempo(tempo),
+            RyvmApp::Add { name, app } => self.add(
+                name.clone(),
+                match app {
+                    AddApp::Number { num } => Instrument::Number(num),
+                    AddApp::Sine { input, voices } => {
+                        let mut instr = Instrument::wave(input, WaveForm::Sine);
+                        if let Some(voices) = voices {
+                            instr = instr.voices(voices);
+                        }
+                        instr
+                    }
+                    AddApp::Square { input, voices } => {
+                        let mut instr = Instrument::wave(input, WaveForm::Square);
+                        if let Some(voices) = voices {
+                            instr = instr.voices(voices);
+                        }
+                        instr
+                    }
+                    AddApp::Mixer { inputs } => Instrument::Mixer(
+                        inputs.into_iter().zip(repeat(Balance::default())).collect(),
+                    ),
+                    #[cfg(feature = "keyboard")]
+                    AddApp::Keyboard { base_octave } => {
+                        Instrument::Keyboard(Keyboard::new(&name, base_octave.unwrap_or(4)))
+                    }
+                    AddApp::Drums => Instrument::DrumMachine(Vec::new()),
+                },
+            ),
+            RyvmApp::Edit {
+                name,
+                set,
+                inputs,
+                volume,
+                pan,
+            } => {
+                if let Some(instr) = self.get_mut(name) {
+                    match instr {
+                        Instrument::Number(n) => {
+                            if let Some(num) = set {
+                                *n = num;
+                            }
+                        }
+                        Instrument::Wave { input, .. } => {
+                            if let Some(new_input) = inputs.into_iter().next() {
+                                *input = new_input;
+                            }
+                        }
+                        Instrument::Mixer(map) => {
+                            if let Some(volume) = volume {
+                                for id in &inputs {
+                                    map.entry(id.clone())
+                                        .or_insert_with(Balance::default)
+                                        .volume = volume;
+                                }
+                            }
+                            if let Some(pan) = pan {
+                                for id in &inputs {
+                                    map.entry(id.clone()).or_insert_with(Balance::default).pan =
+                                        pan;
+                                }
+                            }
+                            for input in inputs {
+                                map.entry(input).or_insert_with(Balance::default);
+                            }
+                        }
+                        #[cfg(feature = "keyboard")]
+                        Instrument::Keyboard(_) => {}
+                        Instrument::DrumMachine { .. } => {}
+                    }
+                }
+            }
+            RyvmApp::Drum {
+                machine,
+                index,
+                path,
+                beat,
+            } => {
+                if let Some(Instrument::DrumMachine(samplings)) = self.get_mut(machine) {
+                    samplings.resize(index + 1, Sampling::default());
+                    if let Some(path) = path {
+                        if let Err(e) = samplings[index].sample.set_path(path) {
+                            println!("{}", e);
+                        }
+                    }
+                    if let Some(be) = beat {
+                        samplings[index].beat = be.parse().unwrap();
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl Iterator for Instruments {
     type Item = SampleType;
     fn next(&mut self) -> Option<Self::Item> {
         // Process commands
-        if self.measure_i() == 0 && self.sample_queue.is_none() {}
+        if self.measure_i() == 0 && self.sample_queue.is_none() {
+            let mut commands = Vec::new();
+            swap(&mut commands, &mut self.command_queue);
+            for app in commands {
+                self.process_command(app);
+            }
+        }
         // Init cache
         let mut cache = FrameCache {
             map: HashMap::new(),
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,7 @@ mod keyboard;
 #[cfg(feature = "keyboard")]
 pub use keyboard::*;
 
-use std::{
-    io::stdin,
-    iter::{once, repeat},
-    sync::mpsc,
-    thread,
-    time::Duration,
-};
+use std::{io::stdin, iter::once, sync::mpsc, thread, time::Duration};
 
 use structopt::StructOpt;
 use unicode_reader::CodePoints;
@@ -39,115 +33,7 @@ fn main() {
             match RyvmApp::from_iter_safe(args) {
                 Ok(app) => match app {
                     RyvmApp::Quit => break,
-                    RyvmApp::Output { name } => {
-                        instruments.update(|instrs| instrs.set_output(name))
-                    }
-                    RyvmApp::Tempo { tempo } => {
-                        instruments.update(|instrs| instrs.set_tempo(tempo))
-                    }
-                    RyvmApp::Add { name, app } => {
-                        instruments.update(|instrs| {
-                            instrs.add(
-                                name.clone(),
-                                match app {
-                                    AddApp::Number { num } => Instrument::Number(num),
-                                    AddApp::Sine { input, voices } => {
-                                        let mut instr = Instrument::wave(input, WaveForm::Sine);
-                                        if let Some(voices) = voices {
-                                            instr = instr.voices(voices);
-                                        }
-                                        instr
-                                    }
-                                    AddApp::Square { input, voices } => {
-                                        let mut instr = Instrument::wave(input, WaveForm::Square);
-                                        if let Some(voices) = voices {
-                                            instr = instr.voices(voices);
-                                        }
-                                        instr
-                                    }
-                                    AddApp::Mixer { inputs } => Instrument::Mixer(
-                                        inputs
-                                            .into_iter()
-                                            .zip(repeat(Balance::default()))
-                                            .collect(),
-                                    ),
-                                    #[cfg(feature = "keyboard")]
-                                    AddApp::Keyboard { base_octave } => Instrument::Keyboard(
-                                        Keyboard::new(&name, base_octave.unwrap_or(4)),
-                                    ),
-                                    AddApp::Drums => Instrument::DrumMachine(Vec::new()),
-                                },
-                            )
-                        });
-                    }
-                    RyvmApp::Edit {
-                        name,
-                        set,
-                        inputs,
-                        volume,
-                        pan,
-                    } => {
-                        instruments.update(|instrs| {
-                            if let Some(instr) = instrs.get_mut(name) {
-                                match instr {
-                                    Instrument::Number(n) => {
-                                        if let Some(num) = set {
-                                            *n = num;
-                                        }
-                                    }
-                                    Instrument::Wave { input, .. } => {
-                                        if let Some(new_input) = inputs.into_iter().next() {
-                                            *input = new_input;
-                                        }
-                                    }
-                                    Instrument::Mixer(map) => {
-                                        if let Some(volume) = volume {
-                                            for id in &inputs {
-                                                map.entry(id.clone())
-                                                    .or_insert_with(Balance::default)
-                                                    .volume = volume;
-                                            }
-                                        }
-                                        if let Some(pan) = pan {
-                                            for id in &inputs {
-                                                map.entry(id.clone())
-                                                    .or_insert_with(Balance::default)
-                                                    .pan = pan;
-                                            }
-                                        }
-                                        for input in inputs {
-                                            map.entry(input).or_insert_with(Balance::default);
-                                        }
-                                    }
-                                    #[cfg(feature = "keyboard")]
-                                    Instrument::Keyboard(_) => {}
-                                    Instrument::DrumMachine { .. } => {}
-                                }
-                            }
-                        });
-                    }
-                    RyvmApp::Drum {
-                        machine,
-                        index,
-                        path,
-                        beat,
-                    } => {
-                        instruments.update(|instrs| {
-                            if let Some(Instrument::DrumMachine(samplings)) =
-                                instrs.get_mut(machine)
-                            {
-                                samplings.resize(index + 1, Sampling::default());
-                                if let Some(path) = path {
-                                    if let Err(e) = samplings[index].sample.set_path(path) {
-                                        println!("{}", e);
-                                    }
-                                }
-                                if let Some(be) = beat {
-                                    samplings[index].beat = be.parse().unwrap();
-                                }
-                            }
-                        });
-                    }
+                    app => instruments.update(|instrs| instrs.queue_command(app)),
                 },
                 Err(e) => println!("{}", e),
             }
